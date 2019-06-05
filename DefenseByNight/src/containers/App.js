2acfdd4e76785f007e7f2799bbb6a1ed91cbc4f8
George French <george.french-sc@airliquide.com>
2019-06-05 11:12:18 +0200

    [EVO] Added IOS Splashscreen

--- DefenseByNight/src/containers/App.js
+++ DefenseByNight/src/containers/App.js
@@ -2,11 +2,16 @@ import React, { Component } from "react";
 import { createAppContainer } from "react-navigation";
 
 import AppNavigator from "../components/AppNavigator";
+import SplashScreen from "react-native-splash-screen";
 
 const AppContainer = createAppContainer(AppNavigator);
 
 type Props = {};
 export default class App extends Component<Props> {
+  componentDidMount() {
+    SplashScreen.hide();
+  }
+
   render() {
     return <AppContainer />;
   }
--- DefenseByNight/src/theme/Images.js
+++ DefenseByNight/src/theme/Images.js
@@ -1,4 +1,6 @@
 export default {
   background: require("../../assets/images/wod.jpg"),
-  profileImage: require("../../assets/images/imageProfile.jpg")
+  profileImage: require("../../assets/images/imageProfile.jpg"),
+  smallWhiteLogo: require("../../assets/images/logo_ys_blanc_petit.svg"),
+  finalLogo: require("../../assets/images/LOGO_YS_final.png")
 };
